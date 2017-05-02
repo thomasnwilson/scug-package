437ef1ce7de10fbc2541974a64971c93c38cd30f
Thomas <thomas-wilson@ouhsc.edu>
2017-05-02 10:37:53 -0500

    Adding integer addition function

---
+++ R/integer-addition.R
@@ -0,0 +1,12 @@
+#' @name integer_addition
+#' @export
+#' @title adds 2 integers together
+#' @description Addition of 2 integers for the purpose of returning a sum (the result of the addition of 2 integers)
+#' @param a The first integer to add
+#' @param b The second integer to add
+integer_addition <- funtion(a, b){
+
+  c <- a + b
+  return(c)
+
+}
